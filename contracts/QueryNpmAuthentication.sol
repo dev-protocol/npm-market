b90be28d6236eae53d344f01c19211a2ba693a49
aggre <hiroyuki.aggre@gmail.com>
2020-01-02 22:31:38 +0900

    separate test contracts

--- contracts/QueryNpmAuthentication.sol
+++ contracts/QueryNpmAuthentication.sol
@@ -38,26 +38,3 @@ contract QueryNpmAuthentication is usingOraclize {
 		NpmMarket(callback).authenticated(_id, _result);
 	}
 }
-
-contract QueryNpmAuthenticationTest is QueryNpmAuthentication {
-	address public owner;
-	event OraclizeQuery(string _f, string _a);
-
-	constructor() public {
-		owner = msg.sender;
-	}
-
-	function oraclize_getPrice() internal pure returns (uint256) {
-		return 1;
-	}
-	function oraclize_query(string memory _f, string memory _a)
-		internal
-		returns (bytes32)
-	{
-		emit OraclizeQuery(_f, _a);
-		return "query_id";
-	}
-	function oraclize_cbAddress() internal returns (address) {
-		return owner;
-	}
-}
