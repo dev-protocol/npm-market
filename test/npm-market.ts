859069c8ef3d34790e28680f99dcd2adbf3e682d
aggre <hiroyuki.aggre@gmail.com>
2020-01-06 10:23:19 +0900

    inplement initialize function

--- test/npm-market.ts
+++ test/npm-market.ts
@@ -1,20 +1,44 @@
 import {waitForMutation} from './utils'
+import {
+	QueryNpmAuthenticationInstance,
+	QueryNpmDownloadsInstance,
+	NpmMarketInstance,
+	MarketInstance
+} from '../types/truffle-contracts'
 const contracts = artifacts.require
+
+const init = async (
+	deployer: string
+): Promise<{
+	queryAuthn: QueryNpmAuthenticationInstance
+	queryDownloads: QueryNpmDownloadsInstance
+	npm: NpmMarketInstance
+	market: MarketInstance
+}> => {
+	const queryAuthn = await contracts('QueryNpmAuthentication').new()
+	const queryDownloads = await contracts('QueryNpmDownloads').new()
+	const npm = await contracts('NpmMarket').new(
+		queryAuthn.address,
+		queryDownloads.address
+	)
+	const market = await contracts('Market').new(npm.address)
+	await Promise.all([
+		queryAuthn.charge({
+			from: deployer,
+			value: '1000000000000000'
+		}),
+		queryDownloads.charge({
+			from: deployer,
+			value: '1000000000000000'
+		})
+	])
+	return {queryAuthn, queryDownloads, npm, market}
+}
 
 contract('NpmMarket', ([deployer]) => {
 	describe('authenticate', () => {
 		it('authenticate npm package and calling Market.authenticatedCallback', async () => {
-			const queryAuthn = await contracts('QueryNpmAuthentication').new()
-			const queryDownloads = await contracts('QueryNpmDownloads').new()
-			const npm = await contracts('NpmMarket').new(
-				queryAuthn.address,
-				queryDownloads.address
-			)
-			const market = await contracts('Market').new(npm.address)
-			await queryAuthn.charge({
-				from: deployer,
-				value: '1000000000000000'
-			})
+			const {market} = await init(deployer)
 			const property = '0x812788B0b58Cb16e7c2DD6Ead2ad2a52a1caFf6F'
 
 			await market.authenticate(
