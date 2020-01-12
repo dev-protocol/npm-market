0e863d67f525ee88805a8ab73f736a21311a778e
aggre <hiroyuki.aggre@gmail.com>
2020-01-12 18:37:59 +0900

    fix failed tests

--- test/migrate-mvp.ts
+++ test/migrate-mvp.ts
@@ -46,7 +46,7 @@ contract('Migrate MVP', ([deployer]) => {
 			)
 		})
 
-		it('migration all packages', async function(done) {
+		it('migration all packages', async function() {
 			this.timeout(3600000)
 			const pkgs: Pkgs = await get({
 				uri: 'https://dev-distribution.now.sh/config/packages',
@@ -67,32 +67,37 @@ contract('Migrate MVP', ([deployer]) => {
 			})
 			console.log('migration is done')
 
-			res.map(async ({metrics, property}) => {
-				const [metricsContract, propertyContract] = await Promise.all([
-					createMetrics(metrics),
-					createProperty(property)
-				])
-				const [
-					related2Metrics,
-					pkgName,
-					author,
-					name,
-					symbol
-				] = await Promise.all([
-					metricsContract.property(),
-					npmMarket.getPackage(metrics),
-					propertyContract.author(),
-					propertyContract.name(),
-					propertyContract.symbol()
-				])
-				const pkg = pkgs.find(x => x.package === pkgName)
-				expect(pkg).to.be.not.equal(undefined)
-				expect(related2Metrics).to.be.equal(property)
-				expect(author).to.be.equal(pkg!.user)
-				expect(name).to.be.match(/^[a-z-]*$/)
-				expect(symbol).to.be.match(/^[A-Z]*$/)
-			})
-			done()
+			await Promise.all(
+				res.map(async ({metrics, property}) => {
+					const [metricsContract, propertyContract] = await Promise.all([
+						createMetrics(metrics),
+						createProperty(property)
+					])
+					const [
+						related2Metrics,
+						pkgName,
+						author,
+						name,
+						symbol
+					] = await Promise.all([
+						metricsContract.property(),
+						npmMarket.getPackage(metrics),
+						propertyContract.author(),
+						propertyContract.name(),
+						propertyContract.symbol()
+					])
+					const pkg = pkgs.find(x => x.package === pkgName)
+					expect(pkg).to.be.not.equal(undefined)
+					expect(related2Metrics.toLocaleLowerCase()).to.be.equal(
+						property.toLocaleLowerCase()
+					)
+					expect(author.toLocaleLowerCase()).to.be.equal(
+						pkg!.address.toLocaleLowerCase()
+					)
+					expect(name).to.be.match(/^[a-z0-9-]*$/)
+					expect(symbol).to.be.match(/^[A-Z0-9]*$/)
+				})
+			)
 		})
 	})
 })
