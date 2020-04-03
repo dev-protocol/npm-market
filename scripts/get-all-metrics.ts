2eddc1298b98294ca96ebccac9f050687d978972
aggre <hiroyuki.aggre@gmail.com>
2020-04-03 13:52:11 +0900

    change graphql endpoint and query

--- scripts/get-all-metrics.ts
+++ scripts/get-all-metrics.ts
@@ -1,10 +1,8 @@
 import ApolloClient, {ApolloQueryResult} from 'apollo-client'
 import gql from 'graphql-tag'
-import nodeFetch, {RequestInfo, RequestInit} from 'node-fetch'
+import fetch from 'node-fetch'
 import {createHttpLink} from 'apollo-link-http'
 import {InMemoryCache} from 'apollo-cache-inmemory'
-import {config} from 'dotenv'
-config()
 
 export type Metrics = {
 	metrics_factory_create: Array<{
@@ -12,28 +10,22 @@ export type Metrics = {
 	}>
 }
 
-const fetch = async (
-	url: RequestInfo,
-	init?: RequestInit | undefined
-): ReturnType<typeof nodeFetch> =>
-	nodeFetch(url, {
-		...init,
-		...{
-			headers: {'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET!},
-		},
-	})
-
 export const getAllMetrics = async (): Promise<ApolloQueryResult<Metrics>> =>
 	new ApolloClient({
 		link: createHttpLink({
-			uri: 'https://devprtcl-event.azurewebsites.net/v1/graphql',
+			uri:
+				'https://dev-protocol-event-viewer.azurewebsites.net/events/v1/graphql?code=8pxrHqDXIvcIw9SiFG0ZL/NaW9lznOpf8dAlvkTtyqeY8wXW38ewdg==',
 			fetch,
 		}),
 		cache: new InMemoryCache(),
 	}).query<Metrics>({
 		query: gql`
 			{
-				metrics_factory_create {
+				metrics_factory_create(
+					where: {
+						from_address: {_eq: "0x88c7B1f41DdE50efFc25541a2E0769B887eB2ee7"}
+					}
+				) {
 					metrics
 				}
 			}
